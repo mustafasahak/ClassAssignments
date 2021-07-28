99116db15b09e50dfc4084cc1644bc03d1f08fee
mustafasahak <msahak@icloud.com>
2021-07-27 20:35:16 -0400

    changed question 18

--- src/question18/largestNumber.java
+++ src/question18/largestNumber.java
@@ -1,22 +1,31 @@
 package question18;
 
+import java.util.Scanner;
+
 public class largestNumber {
 
 	public static void main(String[] args) {
 		
 		
-		int a = 2;
-		int b = 15;
-		int c = 5;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a first number");
+		int a= sc.nextInt();
+		
+		System.out.println("Enter a second number");
+		int b= sc.nextInt();
+		
+		System.out.println("Enter a third number");
+		int c= sc.nextInt();
 
 		if (c > b && c > a) {
-			System.out.println("c is greater");
+			System.out.println(c+ " is greater");
 		}
 		if (b > a && b > c) {
-			System.out.println("b is greater");
+			System.out.println(b+ " is greater");
 		}
 		if (a > c && a > b) {
-			System.out.println("a is greater");
+			System.out.println(a+ " is greater");
 
 		}
 
